7ba83bffbd4f3d24791f977a6a1a3d8f23aa5249
Sittinon SUPAPBURUS <sittinon.s@live.ku.th>
2021-09-07 16:56:42 +0700

    [7/9/64] Commit.

---
+++ Zmedic/Controllers/PatientController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Zmedic.Controllers
+{
+    public class PatientController : Controller
+    {
+        // GET: Patient
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        public ActionResult PatientLabSearch()
+        {
+            return View();
+        }
+
+        public ActionResult LabResultDate()
+        {
+            return View();
+        }
+
+        public ActionResult Result()
+        {
+            return View();
+        }
+    }
+}
