8080851a108703e35d06cca4564065ab01a1a0a4
Sittinon SUPAPBURUS <sittinon.s@live.ku.th>
2021-09-09 18:46:44 +0700

    [9/9/64] Commit.

--- Zmedic/Controllers/PatientController.cs
+++ Zmedic/Controllers/PatientController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Security;
 using System.Web;
 using System.Web.Mvc;
+using Zmedic.Models;
 
 namespace Zmedic.Controllers
 {
@@ -21,61 +22,82 @@ namespace Zmedic.Controllers
             return View();
         }
 
-        public ActionResult LabResultDate()
+        public ActionResult LabResultDate(string dateInput, string sixIdInput)
         {
+            DateTime dateTime = DateTime.Parse(dateInput);
+
+            DateTime dateTimeAddYears = dateTime.AddYears(543);
+
+            string dateOfBirthFileName = dateTimeAddYears.ToString("ddMMyyyy");
+
+            GetFileAndFolderFromSharepoint(dateOfBirthFileName + sixIdInput);
+
             return View();
         }
 
         public ActionResult Result()
         {
-            GetFileAndFolderFromSharepoint();
-
             return View();
         }
 
-        public void GetFileAndFolderFromSharepoint()
+        public void GetFileAndFolderFromSharepoint(string pdfFileName)
         {
             string userName = "sittinon@zmedicgroup.com";
             string password = "1Q2w3e4r";
             var securePassword = new SecureString();
-            foreach (char c in password)
-            {
-                securePassword.AppendChar(c);
-            }
-            using (ClientContext cxt = new ClientContext("https://zmedicgroup.sharepoint.com/sites/ACCRESULT"))
-            {
-                cxt.Credentials = new SharePointOnlineCredentials(userName, securePassword);
-                Web web = cxt.Web;
-                cxt.Load(web, a => a.ServerRelativeUrl);
-                cxt.ExecuteQuery();
-                List list = cxt.Web.Lists.GetByTitle("เอกสาร");
-                cxt.Load(list);
-                cxt.Load(list.RootFolder);
-                cxt.Load(list.RootFolder.Folders);
-                cxt.Load(list.RootFolder.Files);
-                cxt.ExecuteQuery();
-                FolderCollection fcol = list.RootFolder.Folders;
-                List<string> lstFile = new List<string>();
-                List<string> lstFileName = new List<string>();
-                List<string> guestFileUrl = new List<string>();
-                foreach (Folder f in fcol)
-                {
-                    if (f.Name == "COVID")
-                    {
-                        cxt.Load(f.Files);
-                        cxt.ExecuteQuery();
-                        FileCollection fileCol = f.Files;
-                        foreach (File file in fileCol)
-                        {
-                            lstFileName.Add(file.Name);
-                            lstFile.Add("https://zmedicgroup.sharepoint.com" + file.ServerRelativeUrl);
-                        }
-                    }
 
-                    ViewBag.lstFile = lstFile;
-                    ViewBag.lstFileName = lstFileName;
+            try
+            {
+                foreach (char c in password)
+                {
+                    securePassword.AppendChar(c);
                 }
+                using (ClientContext cxt = new ClientContext("https://zmedicgroup.sharepoint.com/sites/ACCRESULT"))
+                {
+                    cxt.Credentials = new SharePointOnlineCredentials(userName, securePassword);
+                    Web web = cxt.Web;
+                    cxt.Load(web, a => a.ServerRelativeUrl);
+                    cxt.ExecuteQuery();
+                    List list = cxt.Web.Lists.GetByTitle("เอกสาร");
+                    cxt.Load(list);
+                    cxt.Load(list.RootFolder);
+                    cxt.Load(list.RootFolder.Folders);
+                    cxt.Load(list.RootFolder.Files);
+                    cxt.ExecuteQuery();
+                    FolderCollection fcol = list.RootFolder.Folders;
+
+                    List<FilePDF> filePDFs = new List<FilePDF>();
+                    foreach (Folder f in fcol)
+                    {
+                        if (f.Name == "COVID")
+                        {
+                            cxt.Load(f.Files);
+                            cxt.ExecuteQuery();
+                            FileCollection fileCol = f.Files;
+                            foreach (File file in fileCol)
+                            {
+                                if (file.Name.StartsWith(pdfFileName))
+                                {
+                                    filePDFs.Add(new FilePDF { FilePdfName = file.Name , FilePdfUrl = "https://zmedicgroup.sharepoint.com" + file.ServerRelativeUrl });
+                                }
+                            }
+                        }
+
+                        if (filePDFs.Count == 0)
+                        {
+                            ViewBag.Nodata = "ไม่พบผลการตรวจ" ;
+                        }
+
+                        ViewBag.lstFile = filePDFs;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                ViewBag.ex = ex;
             }
         }
     }
 }
+
+
---
+++ Zmedic/Models/FilePDF.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Zmedic.Models
+{
+    public class FilePDF
+    {
+        public string FilePdfName { get; set; }
+
+        public string FilePdfUrl { get; set; }
+    }
+}
